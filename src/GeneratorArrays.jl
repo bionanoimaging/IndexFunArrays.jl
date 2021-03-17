811a1d0fc27299f6ac4172d1dc20165d58547472
roflmaostc <fxw+git@mailbox.org>
2021-03-17 21:10:35 +0100

    Add documentation and clean code

--- src/GeneratorArrays.jl
+++ src/GeneratorArrays.jl
@@ -6,15 +6,53 @@ export GeneratorArray
 struct GeneratorArray{T,N,F} <: AbstractArray{T, N} where {F}
     # stores the generator function to be applied to the indices. 
     generator::F     
+    #a offset which is subtracted (.-) from the indices
     offset::NTuple{N, T}
+    #a scaling which is applied after subtraction (.*) s
     scale::NTuple{N, T}
-    size::NTuple{N, Int}   # stores the sizes
+    # output size of the array 
+    size::NTuple{N, Int}
 end
 
 
 # We define three easy constructors
-# * generator function, s
+# * generator function, size
+# * generator, offset, scaling, size
+# * generator, offset, size
+# 
+# And all of the above are available with optional type T
 
+"""
+    GeneratorArray([T], gen::F, size::NTuple{N,Int}) where {N,F}
+
+Generate a GeneratorArray object which behaves like an array
+but does not allocate the full array. Instead it calculates
+the elements when needed. This is useful to prevent array allocations.
+`gen` is a function which takes the array indices wrapped as tuple as input.
+The output of `gen` determines the element type of the resulting array.
+`size` is the output size of the resulting array.
+`T` can be the optional element type of the arrays. 
+`gen` needs to have `T` as return type, otherwise the GeneratorArray
+might be type unstable.
+
+This is the simplest GeneratorArray constructor and only suited for arrays
+containing `<:Numbers`.
+
+# Examples
+```julia-repl
+julia> GeneratorArray(x -> sum(x), (3, 3))
+3×3 GeneratorArray{Int64, 2, var"#182#183"}:
+ 2  3  4
+ 3  4  5
+ 4  5  6
+
+julia> GeneratorArray(x -> sum(abs2.(x)), (3, 3))
+3×3 GeneratorArray{Int64, 2, var"#184#185"}:
+  2   5  10
+  5   8  13
+ 10  13  18
+```
+"""
 function GeneratorArray(gen::F, size::NTuple{N,Int}) where {N,F}
     # type T of the generator array is not provided
     # evaluate it once to get a type
@@ -33,7 +71,66 @@ function GeneratorArray(::Type{T}, gen::F, size::NTuple{N,Int}) where {T, N,F}
                       size);
 end
 
+"""
+    GeneratorArray(gen::F, 
+                   ofs::NTuple{N, G},
+                   size::NTuple{N,Int}) where {N,F,G,H}
 
+`ofs` is a offset subtracted from the array indices.
+
+# Examples
+```julia-repl
+julia> GeneratorArray(x -> sum(x), (1,1), (3, 3))
+3×3 GeneratorArray{Int64, 2, var"#19#20"}:
+ 0  1  2
+ 1  2  3
+ 2  3  4
+```
+"""
+function GeneratorArray(gen::F, 
+                        ofs::NTuple{N, G},
+                        size::NTuple{N,Int}) where {N,F,G,H}
+    T = typeof(gen(size))
+    return GeneratorArray(T, gen, ofs, size) 
+end
+
+function GeneratorArray(::Type{T}, gen::F, 
+                        ofs::NTuple{N, G},
+                        size::NTuple{N,Int}) where {T,N,F,G,H}
+    if typeof(gen(size)) != T
+        error("The generator function does not have Type T as indicated")
+    end
+    ofs = convert(NTuple{N, T}, ofs)
+    sca = ntuple(x -> one(T), length(size))
+    return GeneratorArray(gen, ofs, sca, size) 
+end
+
+"""
+    GeneratorArray(gen::F, 
+                   ofs::NTuple{N, G}, sca::NTuple{N, H}, 
+                   size::NTuple{N,Int}) where {N,F,G,H}
+
+`ofs` is a offset subtracted from the array indices.
+`sca` is a scaling parameter subtracted from the array indices.
+
+This functions is generic and is able to generate non-primitive 
+element array types.
+
+# Examples
+```julia-repl
+julia> GeneratorArray(x -> sum(x), (1,1), (5,5), (3, 3))
+3×3 GeneratorArray{Int64, 2, var"#23#24"}:
+  0   5  10
+  5  10  15
+ 10  15  20
+
+julia> GeneratorArray(x -> x, (1,0), (1,1), (3, 3))
+3×3 GeneratorArray{Int64, 2, var"#39#40"}:
+ (0, 1)  (0, 2)  (0, 3)
+ (1, 1)  (1, 2)  (1, 3)
+ (2, 1)  (2, 2)  (2, 3)
+```
+"""
 function GeneratorArray(gen::F, 
                         ofs::NTuple{N, G}, sca::NTuple{N, H}, 
                         size::NTuple{N,Int}) where {N,F,G,H}
@@ -54,78 +151,21 @@ end
 
 
 
-
-function GeneratorArray(gen::F, 
-                        ofs::NTuple{N, G},
-                        size::NTuple{N,Int}) where {N,F,G,H}
-    T = typeof(gen(size))
-    return GeneratorArray(T, gen, ofs, sca, size) 
-end
-
-function GeneratorArray(::Type{T}, gen::F, 
-                        ofs::NTuple{N, G},
-                        size::NTuple{N,Int}) where {T,N,F,G,H}
-    if typeof(gen(size)) != T
-        error("The generator function does not have Type T as indicated")
-    end
-    ofs = convert(NTuple{N, T}, ofs)
-    sca = ntuple(x -> one(T), length(size))
-    return GeneratorArray(gen, ofs, sca, size) 
-end
-
-
+# define AbstractArray function to allow to treat the generator as an array
+# See https://docs.julialang.org/en/v1/manual/interfaces/#man-interface-array
 Base.size(A::GeneratorArray) = A.size
-Base.similar(A::GeneratorArray, ::Type{T}, size::Dims) where {T} = GeneratorArray(A.generator, A.offset, A.scale, size)
-Base.getindex(A::GeneratorArray{T,N}, I::Vararg{Int, N}) where {T,N} = A.generator(A.scale .* (I .- A.offset))
-Base.setindex!(A::GeneratorArray{T,N}, v, I::Vararg{Int,N}) where {T,N} = error("Attempt to assign to GeneratorArray.")
-
-abstract type Ctr end  # Center of the array
-struct Ctr_Corner <: Ctr end  # corner voxel is zero
-struct Ctr_FFT <: Ctr end # corresponding to FFTs
-struct Ctr_FT <: Ctr end # corresponding to FTs  (meaning shifted FFTs)
-struct Ctr_Mid <: Ctr end # middle of the array
-struct Ctr_End <: Ctr end # other corner voxel is zero
-
-export Ctr,Ctr_Corner,Ctr_FFT,Ctr_FT,Ctr_Mid,Ctr_End
-
-abstract type Sca end # scaling of the array
-struct Sca_Unit <: Sca end # pixel distance is one
-struct Sca_Norm <: Sca end # total size along each dimension normalized to 1.0
-struct Sca_FT <: Sca end # reciprocal Fourier coordinates
-
-export Sca,Sca_Unit,Sca_Norm,Sca_FT
-
-get_offset(size, ::Type{Ctr_Corner}) = size.*0 .+ 1.0
-get_offset(size, ::Type{Ctr_FT}) = size.÷2 .+ 1.0
-get_offset(size, ::Type{Ctr_FFT}) = size.*0 .+ 1.0
-get_offset(size, ::Type{Ctr_Mid}) = (size.+1)./2.0
-get_offset(size, ::Type{Ctr_End}) = size.+0.0
-
-# List of functions and names
-Fkts = [
-    (:(rr2), :(x->sum(abs2.(x)))),
-    (:(rr), :(x->sqrt.(sum(abs2.(x))))),
-    (:(xx), :(x->x[1])),
-    (:(yy), :(x->x[2])),
-    (:(zz), :(x->x[3])),
-    (:(phiphi), :(x->atan.(x[2],x[1]))),
-]
-for F in Fkts
-    @eval $(F[1])(size::NTuple{N,Int}, offset::NTuple{N,Int}, ::Type{T}=Float64,) where{T,N,CT} = GeneratorArray($(F[2]), convert(NTuple{N,T},offset), T, size) 
-    @eval $(F[1])(size::NTuple{N,Int},::Type{CT}=Ctr_FT, ::Type{T}=Float64,) where{T,N,CT} = GeneratorArray($(F[2]), get_offset(size,CT), T, size) 
-    @eval $(F[1])(anArray::AbstractArray{T,N}, offset::NTuple{N,Int},::Type{T}=Float64,) where{T,N,CT} = GeneratorArray($(F[2]), convert(NTuple{N,T},offset), T, size(anArray)) 
-    @eval $(F[1])(anArray::AbstractArray{T,N},::Type{CT}=Ctr_FT,::Type{T}=Float64,) where{T,N,CT} = GeneratorArray($(F[2]), get_offset(size(anArray),CT), T, size(anArray)) 
-    @eval export $(F[1])
-end 
-
-export getPropagator  # This has actually does evaluate. It should be programmed as a point-wise operation
-function getPropagator(k0, dZ) 
-    return x -> exp.(2im.*π.* sqrt.(max.(0.0,k0^2 .- rr2(x,Ctr_FT))))
+Base.similar(A::GeneratorArray, ::Type{T}, size::Dims) where {T} = begin
+    GeneratorArray(A.generator, A.offset, A.scale, size)
 end
 
-#rr(size::NTuple{N,Int},::Type{T}=Float64,::Type{CT}=Ctr_Corner) where{T,N,CT} = GeneratorArray(x->sum(abs2.(x)), get_offset(size,CT), T, size) 
-#rr(size::NTuple{N,Int}, ::Type{CT}=Ctr_Corner) where{N,CT} = rr(size, Float64, CT)
-#rr(anArray::AbstractArray{T,N},::Type{T}) where{T,N,CT} = rr(size(anArray),T,Ctr_Corner) 
-#rr(anArray::AbstractArray{T,N},::Type{CT}=Ctr_Corner) where{T,N,CT} = rr(anArray, Float64,CT)
+# subtract offset and apply scaling afterwards
+Base.getindex(A::GeneratorArray{T,N}, I::Vararg{Int, N}) where {T,N} = begin
+    A.generator(A.scale .* (I .- A.offset))
+end
+
+# not possible
+Base.setindex!(A::GeneratorArray{T,N}, v, I::Vararg{Int,N}) where {T,N} = begin 
+    error("Attempt to assign entries to GeneratorArray which is immutable.")
+end
 
 end # module
