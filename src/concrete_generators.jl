fbd1da4ec0ff7ad3c459563530e87058663297ff
rheintzmann <heintzmann@gmail.com>
2021-03-18 19:20:34 +0100

    Included Window functions

--- src/concrete_generators.jl
+++ src/concrete_generators.jl
@@ -25,13 +25,15 @@ abstract type Sca end # scaling of the array
 struct ScaUnit <: Sca end # pixel distance is one
 struct ScaNorm <: Sca end # total size along each dimension normalized to 1.0
 struct ScaFT <: Sca end # reciprocal Fourier coordinates
+struct ScaFTEdge <: Sca end # such that the edge of the Fourier space is 1.0
 
-export Sca,ScaUnit,ScaNorm,ScaFT
+export Sca,ScaUnit,ScaNorm,ScaFT,ScaFTEdge
 
 get_scale(size, ::Type{ScaUnit}) = ntuple(_ -> one(Int), length(size))
 get_scale(size, ::Type{ScaNorm}) = 1 ./ (size .- 1)
-get_scale(size, ::Type{ScaFT}) = 1 ./ size  # needs revision!
-get_scale(size, t::NTuple) = t  # needs revision!
+get_scale(size, ::Type{ScaFT}) = 0.5 ./ (size .÷ 2)
+get_scale(size, ::Type{ScaFTEdge}) = 1 ./ (size .÷ 2)  
+get_scale(size, t::NTuple) = t 
 
 # List of functions and names we want to predefine
 function generate_functions_expr()
@@ -52,12 +54,22 @@ function generate_functions_expr()
     return functions
 end
 
+function generate_window_functions_expr()
+    # offset and scale is already wrapped in the generator function
+    x_expr = :(scale .* (x .- offset))
+    x_exprW = :(min.(1.0,max.(0,borderI.-abs.(scale .* (x .- offset)))./(borderO-borderI)))
+
+    functions = [
+        (:(hanning),  :(x -> T(prod(sin.(pi/2.0 .* ($x_exprW)).^2)))),
+    ]
+    return functions
+end
+
 # we automatically generate the functions for rr2, rr, ...
 # We set the types for the arguments correctly in the default cases
 for F in generate_functions_expr() 
     # fallback type of GeneratorArray
     default_T = Float64
-
 
     # default functions with certain offset and scaling behavior
     @eval function $(F[1])(::Type{T}, size::NTuple{N, Int};
@@ -75,7 +87,6 @@ for F in generate_functions_expr()
         $(F[1])(T, size, scale=scale, offset=offset) 
     end
 
-
     # convenient wrapper to provide an array as input
     @eval function $(F[1])(arr::AbstractArray{T, N}; 
                            offset=CtrFT, scale=ScaUnit) where {T, N}
@@ -85,9 +96,42 @@ for F in generate_functions_expr()
     @eval export $(F[1])
 end 
 
+# we automatically generate the functions for rr2, rr, ...
+# We set the types for the arguments correctly in the default cases
+for F in generate_window_functions_expr() 
+    # fallback type of GeneratorArray
+    default_T = Float64
+
+    # default functions with certain offset and scaling behavior
+    @eval function $(F[1])(::Type{T}, size::NTuple{N, Int};
+                           offset=CtrFT,
+                           scale=ScaFTEdge, borderI=0.8, borderO=1.0) where{N, T} 
+        offset = get_offset(size, offset)
+        scale = get_scale(size, scale)
+        GeneratorArray(T, $(F[2]), size) 
+    end
+    
+    # change order of offset and scale
+    @eval function $(F[1])(size::NTuple{N, Int}; 
+                           offset=CtrFT, 
+                           scale=ScaFTEdge, borderI=0.8, borderO=1.0) where{N} 
+        T = $default_T 
+        $(F[1])(T, size, scale=scale, offset=offset, borderI=borderI, borderO=borderO) 
+    end
+
+    # convenient wrapper to provide an array as input
+    @eval function $(F[1])(arr::AbstractArray{T, N}; 
+                           offset=CtrFT, 
+                           scale=ScaFTEdge, borderI=0.8, borderO=1.0) where{N, T} 
+        $(F[1])(T, size(arr), scale=scale, offset=offset, borderI=borderI, borderO=borderO)
+    end
+
+    @eval export $(F[1])
+end 
+
 export getPropagator  # This has actually does evaluate. It should be programmed as a point-wise operation
-function getPropagator(k0, dZ) 
-    return x -> exp.(2im.*π.* sqrt.(max.(0.0,k0^2 .- rr2(x,CtrFT))))
+function getPropagator(x, k0, dZ) 
+    return exp.(2im.*π.* sqrt.(max.(0.0,k0^2 .- rr2(x,offset=CtrFT))))
 end
 
 #rr(size::NTuple{N,Int},::Type{T}=Float64,::Type{CT}=CtrCorner) where{T,N,CT} = GeneratorArray(x->sum(abs2.(x)), get_offset(size,CT), T, size) 
