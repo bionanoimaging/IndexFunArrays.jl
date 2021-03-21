3bcfe3902c3aa51cdbed5bcf00294bfcbfaba0da
roflmaostc <fxw+git@mailbox.org>
2021-03-21 16:24:34 +0100

    Add more info to cuda example

--- examples/cuda_example.jl
+++ examples/cuda_example.jl
@@ -22,14 +22,19 @@ function test(s)
     x = rr2(Float32, s)
     x_c = CuArray(rr(Float32, s))
 
+    @info "f on CPU"
     @time f(arr, x);
     @time f(arr, x);
+    @info "f2 on CPU"
     @time f2(arr);
     @time f2(arr);
+    @info "f on GPU"
     CUDA.@time f(arr_c, x);
     CUDA.@time f(arr_c, x);
+    @info "f2 on GPU"
     CUDA.@time f2(arr_c);
     CUDA.@time f2(arr_c);
+    @info "f3 on GPU"
     CUDA.@time f3(arr_c);
     CUDA.@time f3(arr_c);
 
