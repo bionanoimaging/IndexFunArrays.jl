efe2d99d5a6cdddde2bbc18a0b25a7bf5ae9ba8d
roflmaostc <fxw+git@mailbox.org>
2021-04-09 12:37:35 +0200

    Add minimal test for multiple ifa add

--- test/concrete_generators.jl
+++ test/concrete_generators.jl
@@ -21,7 +21,14 @@
     end
 
     @testset "Test rr method" begin
-    
+   
+        offsets = [CtrFFT, CtrFT]
+        scales = [ScaFT, ScaUnit]
+        s = ((7,7))
+        @test rr(s, offset=offsets, scale=scales) ≈ reduce(.+, rr(s, offset=o, scale=sc) for (o,sc) in zip(offsets,scales))
+        
+        @test rr2(s, offset=offsets, scale=scales) ≈ reduce(.+, rr2(s, offset=o, scale=sc) for (o,sc) in zip(offsets,scales))
+
     
         @test rr((5,)) == [2.0, 1.0, 0.0, 1.0, 2.0]
         @test rr(Int, (5,)) == [2, 1, 0, 1, 2]
@@ -34,6 +41,7 @@
         @test rr(Float64, (3, 5), offset = CtrCorner, scale = ScaUnit) == [0.0 1.0 2.0 3.0 4.0; 1.0 1.4142135623730951 2.23606797749979 3.1622776601683795 4.123105625617661; 2.0 2.23606797749979 2.8284271247461903 3.605551275463989 4.47213595499958]
     
         @test rr2(Int, (10, )) == [25; 16; 9; 4; 1; 0; 1; 4; 9; 16]
+        @test rr2(Int, (10, 1)) == reshape([25; 16; 9; 4; 1; 0; 1; 4; 9; 16], (10, 1))
         @test rr2(Int, (10, 1)) == reshape([25; 16; 9; 4; 1; 0; 1; 4; 9; 16], (10, 1))
     end
 
