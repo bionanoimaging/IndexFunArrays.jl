c3d504e793202f947280a1018175e4741991a9dd
Felix Wechsler (he/him) <fxw+git@mailbox.org>
2022-07-06 16:01:22 +0200

    Update docstrings.jl

--- src/docstrings.jl
+++ src/docstrings.jl
@@ -1023,8 +1023,8 @@ A multidimensional box, being one inside and zero outside. `boxsize` defines the
 To obtain box sizes for integer pixels that correspond to the Fourier-space centers both for the array and the box,
 an additional offset of 0.25 pixels is automatically applied and box pixels are one if less or equal to half the boxsize along
 this dimension.
-The default result datatype is Float64.
-```jldoctest
+
+```julia
 julia> box((10,10),(3,6))
 10×10 IndexFunArray{Float64, 2, IndexFunArrays.var"#g#182"{Float64, Tuple{Float64, Float64}, Tuple{Float64, Float64}, Int64}}:
  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0
@@ -1047,7 +1047,8 @@ julia> box((10,10),(3,6))
   0.0  0.0  1.0  1.0  1.0  1.0  0.0  0.0
   0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0
   0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0
- ```
+```
+
 ---
 box(arr::AbstractArray, boxsize; offset=CtrFT, scale=ScaUnit, dims=ntuple(+, N))  
 
