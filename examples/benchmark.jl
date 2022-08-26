904e34d4537298f4e3b8519a72fe4d2e29a7cc83
RainerHeintzmann <heintzmann@gmail.com>
2022-08-26 16:36:54 +0200

    updated benchmarks

--- examples/benchmark.jl
+++ examples/benchmark.jl
@@ -21,17 +21,27 @@ end
 
 function compare_to_CartesianIndices()
     x = ones(1000,1000);
-    qq2(index) = sum(Tuple(index));
-    ww2 = qq2.(CartesianIndices(x));
+    sz = size(x)
+
+    qq2(index) = sum(Tuple(index).^2);
+    rng_start = .-(sz.÷2)
+    rng_stop = rng_start .+ sz .-1
+    rng = Tuple(sta:sto for (sta,sto) in zip(rng_start, rng_stop))
+
+    ci = CartesianIndices(rng);
+    ww2 = qq2.(CartesianIndices(rng));
     y = rr2(x) .+ sqrt.(1.2.*rr2(x).*rr2(x));
 
     @info "rr2 based"
     @btime $y .= rr2($x) .+ sqrt.(1.2.*rr2($x).*rr2($x));
     @btime $y .= rr2($x) .+ sqrt.(1.2.*rr2($x).*rr2($x));
     @info "CartesianIndices based"
-    @btime $y .= ($qq2).(CartesianIndices($x)) .+ sqrt.(1.2.*($qq2).(CartesianIndices($x)).*($qq2).(CartesianIndices($x)));
-    @btime $y .= ($qq2).(CartesianIndices($x)) .+ sqrt.(1.2.*($qq2).(CartesianIndices($x)).*($qq2).(CartesianIndices($x)));
-    @info "CartesianIndices based with initialized function"
+    @btime $y .= ($qq2).(CartesianIndices($rng)) .+ sqrt.(1.2.*($qq2).(CartesianIndices($rng)).*($qq2).(CartesianIndices($rng)));
+    @btime $y .= ($qq2).(CartesianIndices($rng)) .+ sqrt.(1.2.*($qq2).(CartesianIndices($rng)).*($qq2).(CartesianIndices($rng)));
+    @info "precomputed CartesianIndices based"
+    @btime $y .= ($qq2).(ci) .+ sqrt.(1.2.*($qq2).(ci).*($qq2).(ci));
+    @btime $y .= ($qq2).(ci) .+ sqrt.(1.2.*($qq2).(ci).*($qq2).(ci));
+    @info "Applying to precomputed arrays (here generated via CartesianIndices)"
     @btime $y .= $ww2 .+ sqrt.(1.2.*$ww2.*$ww2);
     @btime $y .= $ww2 .+ sqrt.(1.2.*$ww2.*$ww2);
 end 
